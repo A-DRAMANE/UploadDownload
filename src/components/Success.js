6837c8db5c0bdf16a3c4742ef157226a901c4a9b
A-DRAMANE <abdoulcommedramane@gmail.com>
2021-05-26 01:13:47 +0000

    must do drag and drop function and copy link

--- src/components/Success.js
+++ src/components/Success.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { getUrl } from './url'
 import '../css/Success.css'
 import { useHistory } from 'react-router'
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 
 
 
@@ -10,16 +11,30 @@ function Success() {
     if (!getUrl()) {
       history.push("/");
     }
+    const handleCopy = () =>{
+        /* Get the text field */
+        //const copyText = document.querySelector(".myInput");
+
+        /* Select the text field */
+        getUrl().select();
+        getUrl().setSelectionRange(0, 99999); /* For mobile devices */
+
+        /* Copy the text inside the text field */
+        document.execCommand("copy");
+
+        /* Alert the copied text */
+        alert("Copied the text: " + getUrl().value);
+    }
     return (
         <div className="upload">
-            <div>
-                <div>icon</div>
+            <div className="success-top">
+                <CheckCircleIcon></CheckCircleIcon>
                 <h2>Uploaded Successfully</h2>
             </div>
-            <img src={getUrl()}alt="image-upload"/>
-            <div>
-                <div>Link</div>
-                <button>copy Link</button>
+            <img className="img" src={getUrl()}alt="image-upload"/>
+            <div className="success-bottom">
+                <p className="myInput">{getUrl()}</p>
+                <button onClick={handleCopy}>copy Link</button>
             </div>
         </div>
     )
